a6f66b3bcfaad7ce16efca6064bd235ec1dfe291
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2021-06-21 00:25:29 +0530

    Created templates for each page

--- components/sidebar.js
+++ components/sidebar.js
@@ -14,6 +14,7 @@ export default function Sidebar({ children }) {
       <Link href="submission"><a className={styles.sidebar__link}>Submissions</a></Link>
       <Link href="todo"><a className={styles.sidebar__link}>To Do</a></Link>
       <Link href="grades"><a className={styles.sidebar__link}>Grades</a></Link>
+      <Link href="handouts"><a className={styles.sidebar__link}>Handouts</a></Link>
       <Link href="settings"><a className={styles.sidebar__link}>Settings</a></Link>
     </div>
   )
--- pages/submission.js
+++ pages/submission.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import Card from '../components/card'
-import styles from './index.module.scss'
+import Table from '../components/table'
+import styles from './submission.module.scss'
 
 export default function Home() {
 
@@ -11,8 +12,49 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <div className={styles.container}>
-        <Card>
-          
+        <div className={styles.head}>
+          <h6>Submissions {`>`} Math 138</h6>
+          <span>
+            <label className={styles.dropdownLabel} for="sort">Sort</label>
+            <select className={styles.dropdown} name="sort" id="sort">
+              <option value="default">Default</option>
+              <option value="task-type">Task Type</option>
+              <option value="module">Module</option>
+              <option value="due-date">Due Date</option>
+            </select>
+          </span>
+        </div>
+        <Card className={styles.card}>
+          <Table>
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Module</th>
+                <th>Due Date</th>
+                <th>Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Assignment 1</td>
+                <td>Week 1</td>
+                <td>09 June 2021</td>
+                <td>70%</td>
+              </tr>
+              <tr>
+                <td>Assignment 2</td>
+                <td>Week 2</td>
+                <td>16 June 2021</td>
+                <td>89%</td>
+              </tr>
+              <tr>
+                <td>Assignment 3</td>
+                <td>Week 3</td>
+                <td>23 June 2021</td>
+                <td>76%</td>
+              </tr>
+            </tbody>
+          </Table>
         </Card>
       </div>
     </Layout>
