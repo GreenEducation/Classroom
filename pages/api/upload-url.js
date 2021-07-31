9b329288c386488e7f4e451fdd72ee686d771cf0
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2021-07-31 18:43:37 +0530

    uploading using temp signed urls

---
+++ pages/api/upload-url.js
@@ -0,0 +1,77 @@
+import AWS from 'aws-sdk';
+
+export default async function handler(req, res) {
+  AWS.config.update({
+    accessKeyId: process.env.DO_SPACES_KEY,
+    secretAccessKey: process.env.DO_SPACES_SECRET,
+    region: 'nyc3',
+    signatureVersion: 'v4',
+  });
+
+  const s3 = new AWS.S3();
+  const post = await s3.createPresignedPost({
+    Bucket: 'greened-users',
+    Fields: {
+      key: req.query.file,
+    },
+    Expires: 60, // seconds
+    Conditions: [
+      ['content-length-range', 0, 2097152], // up to 2 MB
+    ],
+  });
+
+  res.status(200).json(post);
+}
+
+
+/*
+const spacesEndpoint = new AWS.Endpoint('https://greened-users.nyc3.digitaloceanspaces.com');
+AWS.config.update({
+  accessKeyId: process.env.DO_SPACES_KEY,
+  secretAccessKey: process.env.DO_SPACES_SECRET,
+  region: 'nyc3',
+  endpoint: spacesEndpoint,
+});
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.DO_SPACES_KEY,
+    secretAccessKey: process.env.DO_SPACES_SECRET,
+});
+
+
+
+// Use S3 ManagedUpload class as it supports multipart uploads
+var upload = new AWS.S3.ManagedUpload({
+  params: {
+    Bucket: "greened-users",
+    Key: file.name,
+    Body: file
+  }
+})
+
+var promise = upload.promise()
+promise.then(
+  function(data) {
+    alert("Successfully uploaded photo.")
+  },
+  function(err) {
+    return alert("There was an error uploading your photo: ", err.message)
+  }
+)
+
+*/
+
+
+/*
+var params = {
+  Bucket: "greened-users",
+  Key: file.name,
+  Body: file,
+  ACL: "private"
+};
+
+s3.putObject(params, function(err, data) {
+  if (err) {console.log(err, err.stack);}
+  else     {console.log(data);}
+});
+*/
