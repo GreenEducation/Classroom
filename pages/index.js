41a8bc233a6b96c3be0ec427553e870234741f87
Rayyan Ahmed Quraishi <5747484+rayyanQ@users.noreply.github.com>
2022-11-23 00:01:33 -0500

    Add margin

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
   return (
     <Layout background={registered ? "bg-neutral-900 text-white" : "bg-white text-neutral-900"}>
 
-      <main className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl font-bold">
+      <main className="flex flex-col justify-center items-start mx-2">
+        <h1 className="text-4xl font-bold">
           {registered ? 'We will email you soon. Thanks!' : 'Get all your courses organized into Notion and Google Calendar. $50/term.'}
         </h1><br /><br />
         <input type="text" placeholder="Email Address" className="h-10 w-96 border-2 rounded-full pl-4 text-neutral-900 border-neutral-900" />
-        <button className="underline m-2" onClick={()=>setRegistered(true)}>Submit</button>
+        <button className="underline m-2 font-semibold" onClick={()=>setRegistered(true)}>Submit</button>
       </main>
 
     </Layout>
