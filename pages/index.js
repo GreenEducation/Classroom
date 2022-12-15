197f22068013db8a146b98a13631cc5493c62091
Rayyan Ahmed Quraishi <5747484+rayyanQ@users.noreply.github.com>
2022-12-14 22:39:30 -0500

    Change header text

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className="flex flex-col justify-center items-start mx-2">
         <h1 className="text-4xl font-bold">
-          {registered ? 'We will email you soon. Thanks!' : 'Get all your courses organized into Notion and Google Calendar. $50/term.'}
+          {registered ? 'We will email you soon. Thanks!' : 'Register for early access.'}
         </h1><br /><br />
         <input type="text" placeholder="Email Address" className="h-10 w-96 border-2 rounded-full pl-4 text-neutral-900 border-neutral-900" />
         <button className="underline m-2 font-semibold" onClick={()=>setRegistered(true)}>Submit</button>
